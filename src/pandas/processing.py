46e9b9fb8886d8643761768201e7c79b7160c873
kingkonggrunt <kingkonggrunt@users.noreply.github.com>
2021-09-23 18:20:01 +1000

    feat: data manipulation for Cases (Location) dataset

--- src/pandas/processing.py
+++ src/pandas/processing.py
@@ -23,3 +23,15 @@ def sort_date_value_counts_by_group(df, date, group):
 # ===== Cases (Age Range)
 df = pd.read_csv("data/c19/Cases (Age Range).csv")
 df_age_overtime = sort_date_value_counts_by_group(df, date='notifcation_date', group='age_group')
+
+# ===== Cases (Location)
+df = pd.read_csv('data/c19/Cases (Location).csv')
+
+total_cases = df['notification_date'].value_counts().rename_axis("notification_date").to_frame("count")
+total_postcode = df['postcode'].value_counts().rename_axis("postcode").to_frame("count")
+total_lhd = df['lhd_2010_name'].value_counts().rename_axis("local health district").to_frame("count")
+total_lga = df['lga_name19'].value_counts().rename_axis("local government area").to_frame("count")
+
+postcode_overtime = sort_date_value_counts_by_group(df, 'notification_date', 'postcode')
+lga_overtime = sort_date_value_counts_by_group(df, 'notification_date', 'lga_name19')
+lhd_overtime = sort_date_value_counts_by_group(df, 'notifcation_date', 'lhd_2010_name')
