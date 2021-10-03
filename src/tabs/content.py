e97ad92d611bc365f34a8270cac54e8079f3dff6
kingkonggrunt <kingkonggrunt@users.noreply.github.com>
2021-10-03 17:39:15 +1100

    feat: added datatable for active flights and pt routes

--- src/tabs/content.py
+++ src/tabs/content.py
@@ -1,5 +1,9 @@
 from dash import dcc
 from dash import html
+from dash import dash_table
+import plotly.express as px
+
+from src.data import CovidData
 
 class Tab0Homepage:
     def __init__(self):
@@ -34,14 +38,40 @@ class Tab1:
 class TabActiveRoutes:
     def __init__(self):
 
-        self._title = html.Div(
-            id="tab1",
-            children=html.P(
-                "Active Flight and Public Transport Routes"
-            )
+        self._data = CovidData()
+        self._title = html.P("Active Flight and Public Transport Routes")
+
+    # ===== This is the hard coded stuff for the generation of relavent data
+    def _build_flight_data_table(self):
+        df = self._data.load_csv("Case Flights.csv")
+        table = dash_table.DataTable(
+            id='flights_table',
+            columns=[{"name": i, "id": i} for i in df.columns],
+            data=df.to_dict('records')
         )
+        return table
+
+    def _build_public_transport_data_table(self):
+        df = self._data.load_csv("Case Public Transport Routes.csv")
+        table = dash_table.DataTable(
+            id='public_transport_table',
+            columns=[{"name": i, "id": i} for i in df.columns],
+            data=df.to_dict('records')
+        )
+        return table
+    # =====
 
     def build_child(self):
         return [
-            self._title
+            html.Div(
+            id="tab_active_routes_children",
+            children=[
+                self._title,
+                html.P("Active Flight Routes"),
+                self._build_flight_data_table(),
+                html.P("Active Public Transport Routes"),
+                self._build_public_transport_data_table()
+            ]
+            )
+            #self._build_public_transport_data_table()
         ]
