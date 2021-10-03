370df44027ea1072d9fb3d9fb6b211441b69197c
kingkonggrunt <kingkonggrunt@users.noreply.github.com>
2021-10-03 19:26:12 +1100

    feat: added pie and bar graph for cases by age group

--- src/tabs/content.py
+++ src/tabs/content.py
@@ -24,16 +24,59 @@ class Tab0Homepage:
 class Tab1:
     def __init__(self):
 
-        self._title = html.Div(
-            id="tab1",
-            children=html.P(
-                "Heading for Tab1: Something"
-            )
+        self._data = CovidData()
+        self._title = html.P("Age Groups Data")
+
+    # ===== Graph Building
+    def _bar_total(self):
+        df = self._data.load_csv("Cases (Age Range).csv", parse_dates=['notification_date'])
+        df = processing.value_counts_to_df(df, "age_group", "cases")
+
+        df.sort_index(ascending=True, inplace=True)
+        df.reset_index(inplace=True)
+
+        df['age_group'] = df['age_group'].apply(lambda x: x.replace("AgeGroup_", ""))
+
+        fig = px.bar(df, x="age_group", y="cases",
+            title="Total Cases by Age Group"
         )
+        graph = dcc.Graph(
+            id="age_group_total",
+            figure=fig
+        )
+        return graph
+
+    def _bar_total_normalize(self):
+        df = self._data.load_csv("Cases (Age Range).csv", parse_dates=['notification_date'])
+        df = processing.value_counts_to_df(df, "age_group", "percent", normalize=True)
+
+        df.sort_index(ascending=True, inplace=True)
+        df.reset_index(inplace=True)
+
+        df['percent'] = df['percent'].apply(lambda x: x*100)
+        df['age_group'] = df['age_group'].apply(lambda x: x.replace("AgeGroup_", ""))
+
+        fig = px.pie(df, names="age_group", values="percent",
+            title="Total Cases by Age Group"
+        )
+        fig.update_traces(textposition="inside", textinfo="percent+label")
+        graph = dcc.Graph(
+            id="age_group_total_normalize",
+            figure=fig
+        )
+        return graph
+
 
     def build_child(self):
         return [
-            self._title
+            html.Div(
+                id="tab1",
+                children=[
+                    self._title,
+                    self._bar_total(),
+                    self._bar_total_normalize()
+                ]
+            )
         ]
 
 class TabActiveRoutes:
