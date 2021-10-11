49c98004afa8ab5c362432a8aac15e1e703cf092
kingkonggrunt <kingkonggrunt@users.noreply.github.com>
2021-10-12 10:22:30 +1100

    fix: disabling callback exceptions for callback stacks

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ from src import data
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app.config.suppress_callback_exceptions=True
 server = app.server
 
 # ===== Data Object =====
